00941ecfaacdb90c651d0981b7ff087d722cb414
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2025-10-27 16:43:59 +0100

    fixed minor issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./Styles/App.css";
 import "./Styles/TextStyles.css";
 import "./Styles/ColorVariables.css";
-import { useEffect, useState } from "react";
 import Footer from "./Components/PageParts/Footer";
 import Header from "./Components/PageParts/Header";
 import NavBar from "./Components/PageParts/NavBar";
-import AboutMe from "./Components/Pages/AboutMe";
 
 function App() {
   return (
