f14ec04115df93b7f1a7603f311622d2d25727e8
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2025-10-27 14:37:53 +0100

    added a navbar with styles and added some text styles

--- src/Components/PageParts/NavBar.jsx
+++ src/Components/PageParts/NavBar.jsx
@@ -1,5 +1,51 @@
+import "../../Styles/Nav.css";
+import { useEffect, useState } from "react";
+import { getText } from "../GetCurrentLanguage";
+import { Outlet, Link } from "react-router-dom";
+
 const NavBar = () => {
-  return <h1>Nav bar</h1>;
+  const [NavBarOptions, setNavBarOptions] = useState([{}]);
+  const [chosenOption, setChosenOption] = useState("");
+
+  useEffect(() => {
+    setNavBarOptions(getText("NavBarOptions"));
+    let currentChosenNavBarOption = localStorage.getItem("chosenNavElement");
+    if (currentChosenNavBarOption === "") {
+      setChosenOption(1);
+      localStorage.setItem("chosenNavElement", 1);
+    } else {
+      setChosenOption(currentChosenNavBarOption);
+    }
+  }, []);
+
+  function giveOrTakeChosenId(e, id) {
+    let currentElementId = e.id;
+    if (currentElementId === "") {
+      setChosenOption(id);
+      localStorage.setItem("chosenNavElement", id);
+    }
+  }
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          {NavBarOptions?.map((option, key) => (
+            <Link
+              id={Number(chosenOption) === option.id ? "chosen" : ""}
+              className="li"
+              key={key}
+              to={option.optionDestination}
+              onClick={(e) => giveOrTakeChosenId(e.target, option.id)}
+            >
+              {option.optionName}
+            </Link>
+          ))}
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
 };
 
 export default NavBar;
