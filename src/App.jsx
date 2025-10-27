be995468863bc8af6515f4e30bc4080b93d672d6
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2025-10-27 17:26:03 +0100

    fixed minor issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,21 @@ import "./Styles/ColorVariables.css";
 import Footer from "./Components/PageParts/Footer";
 import Header from "./Components/PageParts/Header";
 import NavBar from "./Components/PageParts/NavBar";
+import { useEffect } from "react";
+
+localStorage.setItem("chosenNavElement", 1);
 
 function App() {
+  useEffect(() => {
+    let currenLocation = window.location.href;
+    let isStartPage = currenLocation[currenLocation.length - 1] === "/";
+
+    if (isStartPage) {
+      localStorage.setItem("chosenNavElement", 1);
+      localStorage.setItem("CurrentLanguage", "dk");
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header />
