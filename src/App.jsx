c00a15ee634abfa5bdd86f3d8ceb88a5ce8f09c1
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2025-10-27 14:44:10 +0100

    added a check for start page to make sure the About me section is only on the start page and the about me page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import "./Styles/App.css";
 import "./Styles/TextStyles.css";
 import "./Styles/ColorVariables.css";
+import { useEffect, useState } from "react";
 import Footer from "./Components/PageParts/Footer";
 import Header from "./Components/PageParts/Header";
 import NavBar from "./Components/PageParts/NavBar";
 import AboutMe from "./Components/Pages/AboutMe";
 
 function App() {
+  const [currenLocation, setCurrenLocation] = useState("");
+  const [isStartPage, setIsStartPage] = useState(true);
+
+  useEffect(() => {
+    let location = window.location.href;
+    setCurrenLocation(location);
+    setIsStartPage(currenLocation[currenLocation.length - 1] === "/");
+    if (isStartPage) {
+      localStorage.setItem("chosenNavElement", 1);
+      localStorage.setItem("CurrentLanguage", "dk");
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <NavBar />
-      <AboutMe />
+      {isStartPage && <AboutMe />}
       <Footer />
     </div>
   );
