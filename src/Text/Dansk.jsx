09818bae7d4ec51e4264816360242b342e549d37
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2025-10-27 13:53:32 +0100

    Added files with the text I will be using

---
+++ src/Text/Dansk.jsx
@@ -0,0 +1,129 @@
+export const HeaderHeading_dk = "Full-stack udvikler";
+export const HeaderContent_dk =
+  "Jeg elsker at arbejde med begge front-end og back-end programmering";
+
+export const AboutMeHeadingAndParagraph_dk = {
+  heading: "Om Mig",
+  paragraphs: [
+    "Jeg er en islænding men blev født I Danmark og har boet her den største delen af mit liv og jeg er her for at blive. Jeg er en uddannet Full-stack udvikler (back-end og front-end udvikler) som elsker at kode. Jeg er en nørd med en passion for at lære nye ting og med kode ved jeg at man løber aldrig tør for nye ting at lære.",
+    "Jeg fortrækker at gennemtænke min funktionalitet før jeg starter med at kode, så jeg kan få den reneste og smarteste kode muligt. Jeg kan bedst lide at arbejde Agilt, hvor alt er planlagt og jeg kan også lide da der kommer nye ændringer, midt i projektet. Jeg elsker at tænke gennem hvordan jeg gør min kode så nem som muligt at ændre i fremtiden, da alt teknologi opdateres regelmæssigt og derfor bliver alt kode ændret på et tidspunkt i fremtiden. Jeg er ærlig, venlig, kreativ og er god til at være upartisk og fair. Jeg arbejder godt alene og som en del af en gruppe. Jeg har meget erfaring med at forklare kode relaterede ting til mennesker som ved lidt eller intet om kode eller computere i det hele taget.",
+    "Jeg har altid været god til at huske; jeg har endda lært et sæt spillekort udenad én gang i løbet af to dage. Nogle tal sidder endda fast for livet, som for eksempel dette nummer 0118 999 88199 9119 725 3 fra et tv-program kaldet 'The IT crowd', som I kan godt spørge om i et interview.",
+    "Udenover kodning kan jeg lide at male, bage og at spille bræt-, kort- og computerspil. Jeg kan også lide at tage billeder, her er nogle af mine billeder som jeg tog under en skole tur til Jordan",
+  ],
+};
+
+export const ExperienceHeadingsAndParagraphs_dk = [
+  {
+    location: "Furesø kommunes",
+    position: "Full-stack udvikler",
+    date: "(August 2024 – Oktober 2024)",
+    paragraph: [
+      "Jeg startede mit praktikforløb hos Furesø kommunes digitaliserings afdeling, den 26ende august og skulle være der i 4 uger, men de bad mig om at blive i 4 uger til, så jeg kunne færdiggøre min opgave og kigge på en anden opgave til. I starten af praktikforløbet fik jeg en Excel fil og blev bedt om at gøre indholdet søgbart. Der var ikke så mange detaljer i starten da de var ikke helt sikker hvad det var de manglede, så hver uge mødtes jeg med en lille gruppe som gav bedre indblik i hvad gav mening i forhold til hvad det var de manglede. Jeg byggede brugergrænsefladen i ReactJS og til et møde fandt de så ud af at alle filerne som man skulle kunne downloade, var i mange forskellige filformater med de skulle alle være i PDF-format, så jeg lavede et lille program i Python for at konvertere dem. Da jeg blev færdig med opgaven, havde jeg lidt mere tid så jeg lavede også et lille Python program som kombinerede nogle Excel filer baserede på et par kolonner.",
+      "Karen Thestrup Cuppers (Webmaster) har skrevet en anmeldelse på LinkedIn: Jeg har haft fornøjelse af at arbejde sammen med Inga i Furesø Kommune. Inga har på kort tid kortlagt og eksekveret et større dataprojekt, som ligeledes blev udbygget undervejs som en naturlig del af projektets udvikling og interessenters input. Stor tak til Inga for hendes omstillingsparathed og gode samarbejde. ",
+    ],
+  },
+  {
+    location: "Furesø kommunes",
+    position: "RPA udvikler (Robotbaseret procesautomatisering)",
+    date: "(December 2024 – Juni 2025)",
+    paragraph: [
+      "Kort efter min praktik sluttede blev jeg kontaktet af Furesø kommune igen for en midlertidig stilling for at hjælpe deres RPA udvikler. Under min anden uge der havde jeg fået styr på at lave og opdatere robotter i Power automate, resten af min første 4 uger blev jeg introduceret til deres vigtigste robotter. Efter min fjerde uge stoppede deres RPA udvikler og efter det skulle jeg holde styr på deres 90 robotter alene. Jeg arbejdede for det meste i Power automate, men brugte regelmæssigt HTML og CSS for at formatere mails og Python scripts for de mere komplicerede funktioner.",
+      "Min chef der, Christian Bo Christiansen har skrevet en anmeldelse på LinkedIn: Jeg har været leder for og arbejdet sammen med Inga i Furesø Kommune. Inga vedligeholdte og udviklede RPA- løsninger i økonomiafdelingen. Inga havde begrænset erfaring med RPA, da hun startede, men lærte meget hurtigt at løse opgaverne med den daglige drift og udvikling af nye løsninger i Power Automate. Inga var ansvarsfuld omkring sine opgaver, god at arbejde sammen med og hun deltog også gerne afdelings sociale liv. Hvis du/I har Inga i spil til et job, så tag gerne kontakt.",
+    ],
+  },
+  {
+    location: "Danmarks Meteorologiske Institut",
+    position: "Front-End udvikler",
+    date: "(Februar 2023 - Juni 2023)",
+    paragraph: [
+      "Som en del af min Datamatiker uddannelse tog jeg praktik og hovedopgaveforløb hos DMI. Der arbejdede jeg på at opdatere en af deres hjemmesider som hedder vejvejr, da deres forrige version var blevet forældet. Den nye version blev lavet som en Single Page Application lavet med ReactJS, samt mange frameworks, værktøjer og komponent libraries og med GitLab som hosting platform.",
+      "Under praktikperioden lavede jeg og min praktik partner mulighed for at lave overvågning af brugerdefinerede alarmer. Under hovedopgaveforløbet brugte jeg og min praktik partner moderne visualiserings værktøjer til at fremvise vejrstationer, beliggende forskellige steder rundt i Danmark samt deres parametre (tværvind og vindstød) på et kort. I enden af forløbet fik vi en meget god respons fra DMI, samt vores lærer og censor under eksamener.",
+      "Anne Marie Munk Jørgensen, leder af Rådgivning og Digitalisering hos DMI var min vejleder i praktikperioden og vil meget gerne give reference til dem der ønsker.",
+    ],
+  },
+];
+
+export const EducationHeadingAndParagraphs_dk = [
+  {
+    location: "CPH-Business i Lyngby",
+    position: "Datamatiker",
+    date: "(Januar 2021 – Juni 2023)",
+    paragraph: [
+      "I løbet af min datamatikeruddannelse lærte jeg Java, SQL, oprettelse af REST API'er ved hjælp af Java Persistence API (JPA) og MYSQL, Unit and integration tests, Python, CD/CI, Maven, Docker, Apache Tomcat, de agile metoder, Extreme programming (XP) og Git (GitHub) i dybden. Jeg har brugt alle disse i mange forskellige projekter, normalt gennem IntelliJ IDEA, men jeg brugte også NetBeans et par gange. Jeg blev også introduceret til flere forskellige frameworks, libraries, værktøjer og sprog. Min uddannelse fokuserede på at hurtigt lære nye teknologier at kende og at bruge dem i mangle forskellige projekter.",
+      "Da det kom til eksamen i JPA lavede jeg et øvelsesprojekt som havde OneSide, OtherOneSide, ManySide, OtherManySide entities/Tables/Schemas, så jeg kunne lave alle de mulige relationer one to one, one to many and many to many. Her er linket til koden https://github.com/Inga-Helgadottir/JPA-Notes",
+      "Mit Python eksamensprojekt var at lavet spillet Hangman, hvor man skulle gætte på enten Rick and Morty karakterers navn som jeg fik fra et gratis API jeg fandt online eller film navne fra en CSV-fil jeg fandt online. Jeg brugte også et Python library som hedder Pillow og kan hjælpe med UI delen af spillet. Her kan du se koden bag hangman er her: https://github.com/Inga-Helgadottir/PythonEksamensProjekt",
+    ],
+  },
+  {
+    location:
+      "Hovedforløb hos Roskilde Tekniske skole og Grundforløb hos NEXT-uddannelse i København",
+    position: "Webudvikler",
+    date: "(Januar 2019 – Oktober 2020)",
+    paragraph: [
+      "Under webudviklingsuddannelsen lærte jeg HTML, CSS, SASS, Bootstrap, JavaScript, Git (GitHub), JSON og hvordan man bruger REST API'er i dybden, ved at bruge hver af dem sammen i mange forskellige projekter ved hjælp af Visual Studio Code. Jeg blev også introduceret til flere forskellige frameworks, libraries, værktøjer og sprog.",
+      "Hver uge lavede vi en hjemmeside som handlede om vores yndlingsfilm, spil, album osv. Et af de mest mindeværdige projekter, jeg arbejdede på, var 40'ernes projektet. Der blev undervist i mange forskellige uddannelser i NEXT, og vi var alle delt op i teams, hvert hold med en front-end udvikler, UI/UX-designer, et par kostumedesignere, et par makeupartister og fotografer. Hvert hold fik et årti, og vi fik 40'erne. Vi arbejdede alle som et team for at lave en hjemmeside dedikeret til vores årti, som desværre ikke længere er online. For at få en bedre ide om, hvad vi har lært, kan du tage et kig på øvelsesarkivet, jeg oprettede for at blive klar til eksamen: https://github.com/SIngaH/practice.",
+    ],
+  },
+  {
+    location:
+      "Jeg har bestået Engelsk A og Dansk A hos VUC Lyngby og Matematik B hos KVUC.",
+    position: "Dansk A, Engelsk A og Matematik B",
+    date: "(December 2017 –December 2018)",
+    paragraph: [""],
+  },
+];
+
+export const CoursesHeadingAndParagraphs_dk = [
+  {
+    location: "ExOpi Talent Academy",
+    position: "",
+    date: "(April 2024 – Juni 2024)",
+    paragraph: [
+      "ExOpi Talent Academy er en 10-ugers kursus hvor jeg arbejdede på forskellige projektere til at vise dem hvad jeg kan imens jeg lærer nye ting og de hjælper med at finde et job til mig. Nogle af de opgaver jeg arbejdede på, var at lave spillet Blackjack ud af ReactJS og et Python projekt som kigger på en CSV-fil fyldt med linker til PDF filer, projektet kigger på disse links og hvis de er valide PDF-filer, skulle den downloade filen og hvis ikke bliver det link og informationer om hvorfor den ikke virker i en anden CSV-fil. I dette projekt brugte jeg multithreading for at kunne gennemgå de 250 links givet på 2 minutter i stedet for 3 timer, som det tog før multithreading. For at se koden bag disse 2 projekter kan du kigge på: https://github.com/Inga-Helgadottir/ExopiTalentAcademyProjects/, de andre projekter er privat, da det indeholder sensitive informationer.",
+      "Jonas Rasmusen er lederen af Talentakademi og Innovation hos ExOpi og Bina Hjorth Reichstein er deres CEO og de er begge klar til at give en reference hvis I vil have den.",
+    ],
+  },
+  {
+    location: "IT-sikkerheds kursus - CompTIA Security+",
+    position: "",
+    date: "(December 2023 – Januar 2024)",
+    paragraph: [
+      "Her lærte jeg om komponenterne i en computer sammen med mulige tilføjelser og opdatering og udskiftning af disse komponenter. Håndtering af Windows-, MacOS- og Linux-enheder gennem indstillinger, kontrolpanel og kommandolinjegrænsefladen. Trådløse sikkerhedsimplementeringer, metoder til forebyggelse og fjernelse af malware, mobile sikkerhedsløsninger, bedste praksis for dokumentation, privatliv, licens- og politikkoncepter, korrekte kommunikationsteknikker og professionalisme, scriptteknikker, fjernadgangsmetoder og meget mere.",
+    ],
+  },
+  {
+    location: "Kurser jeg har taget på LinkedIn Learning",
+    position: "",
+    date: "",
+    paragraph: [
+      "TypeScript Essential Training - https://lnkd.in/eS6tde62",
+      "Azure AI: The Big Picture (2023) - https://lnkd.in/eEpDfyzt ",
+      "Microsoft Power Automate Essential Training - https://lnkd.in/esz2VXU3 ",
+      "From React to React Native - https://lnkd.in/eEc-F4yJ",
+      "Microsoft Power Platform App Maker (PL-100) Cert Prep - https://lnkd.in/e_z-CkRi",
+    ],
+  },
+];
+
+export const NavBarOptions_dk = [
+  {
+    id: 1,
+    optionName: "Om Mig",
+    optionDestination: "/AboutMe",
+  },
+  {
+    id: 2,
+    optionName: "Uddannelse",
+    optionDestination: "/Education",
+  },
+  {
+    id: 3,
+    optionName: "Mine kurser",
+    optionDestination: "/Courses",
+  },
+  {
+    id: 4,
+    optionName: "Erfaring",
+    optionDestination: "/Experience",
+  },
+];
---
+++ src/Text/English.jsx
@@ -0,0 +1,131 @@
+export const HeaderHeading_eng = "Full-stack developer";
+export const HeaderContent_eng =
+  "I love working with both front-end and back-end programming";
+
+export const AboutMeHeadingAndParagraph_eng = {
+  heading: "About me",
+  paragraphs: [
+    "I am an educated Full-Stack developer (back-end and front-end developer) who loves coding. I am a nerd at heart with a passion for coding and learning new things and I know when you work with code, you will never run out of new things to learn. My main goal when coding is to make it easy for the user to understand without having to read any documentation to understand how to use the application. I also like to make my code easy to update in the future, when new features need to be added or when new and better technologies become available. ",
+    "I prefer to think through the functionality before I start coding, so I get the best and cleanest code for the purpose. I like working with Agile methodologies, where everything is planned, and I even like getting a surprise new feature in the middle of a project. I love thinking through how to make my code as easy to update as possible, as all technology keeps growing, getting safer and easier to use, which means all code will eventually be changed.",
+    "I am an honest, kind, and creative person, who is good at being impartial and fair. I work well both independently and as a part of a group. I have had a lot of practice explaining code related things to people who know nothing about code or even computers in general.",
+    "I have always been good at memorization; I have even memorized a deck of cards in the span of two days. Some numbers even get stuck for life, like this number 0118 999 88199 9119 725 3 from a TV show called “The IT crowd” ask me about it in an interview.",
+    "Outside of coding I enjoy hanging out with friends, painting, baking, and playing board, card and computer games. I also like to take pictures, here are some I took during a school trip to Jordan",
+  ],
+};
+
+export const ExperienceHeadingsAndParagraphs_eng = [
+  {
+    location: "Furesø kommune",
+    position: "Full-stack developer",
+    date: "(August 2024 – October 2024)",
+    paragraph: [
+      "I started an internship at Furesø kommune digitization department on the 26th of August and was supposed to stay there for 4 weeks, but they asked me to stay another 4 weeks. Even though they don’t have enough coding projects to give me a job, they did have a few possible projects, one of which was making a very large CSV file easier to search through, as searching through the existing file was confusing and time consuming. I built the user interface for this data in ReactJS, I used Python to alter multiple different filetypes to pdfs files. I also used Python to join some Excel files together based on a few shared columns.",
+      "Karen Thestrup Cuppers (Webmaster) LinkedIn review: I have had the pleasure of working with Inga in Furesø Kommune. Inga has mapped out and executed a major data project in a short time, which was also expanded along the way as a natural part of the project's development and stakeholder input. Many thanks to Inga for her adaptability and good collaboration.",
+    ],
+  },
+  {
+    location: "Furesø kommune",
+    position: "RPA developer (Robotic Process Automation)",
+    date: "(December 2024 – June 2025)",
+    paragraph: [
+      "Shortly after my internship ended, I was contacted by Furesø kommune for a temporary position helping their RPA developer (Robotic process automation). For the first 4 weeks I got a crash course in Power automate and some of the more important robots. By the second week I had gotten the hang of building and updating Power automate robots. The rest of the time I was taught about their most important robots, at the end of those 4 weeks their RPA developer quit, so I was on my own maintaining their many robots for months. I worked a lot in Power automate and regularly used HTML and CSS to format emails and Python scripts for the more advanced functions, that can be difficult to complete in Power automate alone.",
+      "My boss there, Christian Bo Christiansen LinkedIn review: I have been a manager for and worked with Inga in Furesø kommune. Inga maintained and developed RPA solutions in the finance department. Inga had limited experience with RPA when she started, but learned very quickly to solve the tasks with the daily operation and development of new solutions in Power Automate. Inga was responsible for her tasks, good to work with and she also enjoyed participating in the department's social life. If you have Inga in mind for a job, please get in touch.",
+    ],
+  },
+  {
+    location: "Danish Meteorological Institute",
+    position: "Front-End developer",
+    date: "(February 2023 - June 2023)",
+    paragraph: [
+      "As a part of my Computer Science education, me and one of my classmates got an internship at DMI. We worked on updating one of their websites called VejVejr as the old version had become outdated. This new version was a Single Page Application created with ReactJS and multiple frameworks and component libraries and as a code hosting platform, we used GitLab. We added functionality for monitoring user defined alarms and choosing how you are notified, either on the site, via SMS and/or email. For example, you could get notified if there is frost at a particular weather station, so you know when to salt the roads near that station.",
+      "We continued working for DMI through our main project as well, on the same site but a different tab. This time we worked on a tab that shows a map of all the weather stations in Denmark. There we used modern visualization tools to add two new parameters crosswind and wind gust to the map, showing the strength of the crosswind and wind gust, along with a graph over the readings from the last few hours at this station.",
+      "At the end of our main project, we showed the result of our work at a meeting with people from many different positions within DMI. The response we received was very positive, both from DMI’s employees and our teacher and censor during the exam.",
+      "Anne Marie Munk Jørgensen is the leader of Consulting and Digitization at DMI and my mentor during the internship period and is willing to give a recommendation to those who want it.",
+    ],
+  },
+];
+
+export const EducationHeadingAndParagraphs_eng = [
+  {
+    location: "CPH-Business in Lyngby",
+    position: "Computer science",
+    date: "(January 2021 – June 2023)",
+    paragraph: [
+      "During my Computer Science education, I learned Java, SQL, creating REST APIs using Java Persistence API (JPA) and MYSQL, CI/CD Unit and integration tests, Python, Maven, Docker, Apache Tomcat, the Agile methodologies, Extreme programming (XP) and Git (GitHub) in depth. I have used all of these in many different projects, usually through IntelliJ IDEA but I also used NetBeans a few times. I was also introduced to multiple different frameworks, libraries, tools, and languages. My education was centered around me being able to quickly learn new languages, tools, and technologies and use them in various different projects.",
+      "My exam project in Python was to make Hangman, using the Rick and Morty API and some Movie data in csv format as the guess words. Then I used Pillow, which is a python library to make the UI part of the hangman game. This is the repository of that project https://github.com/Inga-Helgadottir/PythonEksamensProjekt",
+      "To get a better idea of my skills you can also check out the practice project I created before my exam in JPA. This project has the entities/Tables/Schemas OneSide, OtherOneSide, ManySide, OtherManySide, so that I could get all the different relationships one to one, one to many and many to many. This is the repository where I practiced for my exam https://github.com/Inga-Helgadottir/JPA-Notes",
+    ],
+  },
+  {
+    location:
+      "I passed the Main course at Roskilde Technical school and the Basic course at NEXT in Copenhagen",
+    position: "Web Developer ",
+    date: "(January 2019 – October 2020)",
+    paragraph: [""],
+  },
+  {
+    location:
+      "I passed English A and Danish A at VUC Lyngby and Math B at KVUC.",
+    position: "Danish A, English A and Math B ",
+    date: "(December 2017 –December 2018)",
+    paragraph: [
+      "During the Web development education, I learned HTML, CSS, SASS, Bootstrap, JavaScript, Git (GitHub), JSON and how to use REST APIs in depth, using each of them together in many different projects using Visual Studio Code. I was also introduced to multiple different frameworks, libraries, tools, and languages.",
+      "We had a new project each week, making a website for our favorite Movies, Bands, Games and so on. One of the most memorable projects I worked on was the 40’s project. Many different educations were being taught in NEXT, and we were all divided into teams, each team with a front-end developer, UI/UX designer, a few costume designers, a few make-up artists and photographers. Each team was given a decade, and we got the 40’s. We all worked as a team to make a website dedicated to our decade, I worked closely with the UX/UI designer to make our website which sadly is no longer online. To get a better idea of what we learned you can take a look at the practice repository, I created to get ready for the exam: https://github.com/SIngaH/practise.",
+    ],
+  },
+];
+
+export const CoursesHeadingAndParagraphs_eng = [
+  {
+    location: "ExOpi Talent Academy",
+    position: "",
+    date: "(April 2024 – June 2024)",
+    paragraph: [
+      "ExOpi Talent Academy is a 10-week course where we worked on projects to show what we can do, learn new skills and refine our own, while they also help us find a job. Some examples of my tasks during the course were making Blackjack out of ReactJS and a Python project that looks at links in a CSV file and if they are valid PDF files, they are downloaded and information about each link is saved in another CSV file even if it was not a valid PDF link. For this project I also used multiple threads to cut the download time for all 250 of the links provided from 3 hours to 2 minutes. To see some of these projects you can look at my repository: https://github.com/Inga-Helgadottir/ExopiTalentAcademyProjects/, there are only 2 projects on this one but the other has to remain private as it contains sensitive information.",
+      "Jonas Rasmusen is the head of Talent Academy and Innovation at ExOpi and Bina Hjorth Reichstein is their CEO, and they are both ready to give a reference if you want it.",
+    ],
+  },
+  {
+    location: "Course in IT security - CompTIA Security+",
+    position: "",
+    date: "(December 2023 – January 2024)",
+    paragraph: [
+      "Here I learned about the components of a computer along with possible add-ons and update and replacement of those components. I also learned preventative measures to ensure the safety of a device, along with the detection and removal of malware. Managing Windows, MacOS and Linux devices, through settings, control panel and the command line interface. Wireless security implementations, mobile security solutions, best documentation practices, privacy, licensing & policy concepts, proper communication techniques and professionalism, scripting techniques, remote access methods and a lot more.",
+    ],
+  },
+  {
+    location: "Courses I've taken on LinkedIn Learning",
+    position: "",
+    date: "",
+    paragraph: [
+      "TypeScript Essential Training - https://lnkd.in/eS6tde62",
+      "Azure AI: The Big Picture (2023) - https://lnkd.in/eEpDfyzt ",
+      "Microsoft Power Automate Essential Training - https://lnkd.in/esz2VXU3 ",
+      "From React to React Native - https://lnkd.in/eEc-F4yJ",
+      "Microsoft Power Platform App Maker (PL-100) Cert Prep - https://lnkd.in/e_z-CkRi",
+    ],
+  },
+];
+
+export const NavBarOptions_eng = [
+  {
+    id: 1,
+    optionName: "About me",
+    optionDestination: "/AboutMe",
+  },
+  {
+    id: 2,
+    optionName: "Experience",
+    optionDestination: "/Experience",
+  },
+  {
+    id: 3,
+    optionName: "Education",
+    optionDestination: "/Education",
+  },
+  {
+    id: 4,
+    optionName: "Courses",
+    optionDestination: "/Courses",
+  },
+];
