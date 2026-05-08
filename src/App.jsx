369b1ea537834caac2f2a42970f0a634e1855d67
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-05-08 10:13:44 +0200

    moved routes from index to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,60 @@ import "./Styles/ColorVariables.css";
 import Footer from "./Components/PageParts/Footer";
 import Header from "./Components/PageParts/Header";
 import NavBar from "./Components/PageParts/NavBar";
-import { useEffect } from "react";
+import { useState } from "react";
 import BackToTheTopComponent from "./Components/PageParts/BackToTheTopComponent";
+import { getCurrentLanguage } from "./Components/GetCurrentLanguage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutMe from "./Components/Pages/AboutMe";
+import MyCourses from "./Components/Pages/MyCourses";
+import MyEducation from "./Components/Pages/MyEducation";
+import MyExperience from "./Components/Pages/MyExperience";
+import NoPage from "./Components/Pages/NoPage";
 
-localStorage.setItem("chosenNavElement", 1);
-
-function App() {
-  useEffect(() => {
-    let currenLocation = window.location.href;
-    let isStartPage = currenLocation[currenLocation.length - 1] === "/";
-
-    if (isStartPage) {
-      localStorage.setItem("chosenNavElement", 1);
-      localStorage.setItem("CurrentLanguage", "dk");
-    }
-  }, []);
+const App = () => {
+  const [currentLanguage, setCurrentLanguage] = useState(getCurrentLanguage());
 
   return (
-    <div className="App" id="top">
-      <Header />
-      <NavBar />
-      <BackToTheTopComponent />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App" id="top">
+        <Header
+          currentLanguage={currentLanguage}
+          setCurrentLanguage={setCurrentLanguage}
+        />
+        <NavBar currentLanguage={currentLanguage} />
+
+        <Routes>
+          <Route
+            index
+            element={<AboutMe currentLanguage={currentLanguage} />}
+          />
+          <Route
+            path="AboutMe"
+            element={<AboutMe currentLanguage={currentLanguage} />}
+          />
+          <Route
+            path="Courses"
+            element={<MyCourses currentLanguage={currentLanguage} />}
+          />
+          <Route
+            path="Education"
+            element={<MyEducation currentLanguage={currentLanguage} />}
+          />
+          <Route
+            path="Experience"
+            element={<MyExperience currentLanguage={currentLanguage} />}
+          />
+          <Route
+            path="*"
+            element={<NoPage currentLanguage={currentLanguage} />}
+          />
+        </Routes>
+
+        <BackToTheTopComponent currentLanguage={currentLanguage} />
+        <Footer currentLanguage={currentLanguage} />
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
