42c2079d01b2bab966162c6948c3eae10dcf57f9
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-06-29 15:27:44 +0200

    code clean up

--- src/Components/PageParts/Section.jsx
+++ src/Components/PageParts/Section.jsx
@@ -3,23 +3,22 @@ import "../../Styles/Sections.css";
 const Section = (element) => {
   return (
     <div>
-      {element.option.length > 1 &&
-        element.option?.map((section, key) => {
-          return (
-            <section key={key} className="section">
-              <h3>{section.location}</h3>
-              <p>{section.position}</p>
-              <p>{section.date}</p>
-              {section.paragraph?.map((paragraph, key) => {
-                return (
-                  <div className="sectionParagraph" key={key}>
-                    {paragraph}
-                  </div>
-                );
-              })}
-            </section>
-          );
-        })}
+      {element.option?.map((section, key) => {
+        return (
+          <section key={key} className="section">
+            <h3>{section.location}</h3>
+            <p>{section.position}</p>
+            <p>{section.date}</p>
+            {section.paragraph?.map((paragraph, key) => {
+              return (
+                <div key={key} className="sectionParagraph">
+                  {paragraph}
+                </div>
+              );
+            })}
+          </section>
+        );
+      })}
     </div>
   );
 };
