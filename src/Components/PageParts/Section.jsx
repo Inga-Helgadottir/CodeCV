3cae28d0f6bee2498012e4a6f69c6a8c94c288bb
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-07-03 10:51:50 +0200

    fixed bug

--- src/Components/PageParts/Section.jsx
+++ src/Components/PageParts/Section.jsx
@@ -3,22 +3,23 @@ import "../../Styles/Sections.css";
 const Section = (element) => {
   return (
     <div>
-      {element.option?.map((section, key) => {
-        return (
-          <section key={key} className="section">
-            <h3>{section.location}</h3>
-            <p>{section.position}</p>
-            <p>{section.date}</p>
-            {section.paragraph?.map((paragraph, key) => {
-              return (
-                <div key={key} className="sectionParagraph">
-                  {paragraph}
-                </div>
-              );
-            })}
-          </section>
-        );
-      })}
+      {element.option.length > 1 &&
+        element.option?.map((section, key) => {
+          return (
+            <section key={key} className="section">
+              <h3>{section.location}</h3>
+              <p>{section.position}</p>
+              <p>{section.date}</p>
+              {section.paragraph?.map((paragraph, key) => {
+                return (
+                  <div key={key} className="sectionParagraph">
+                    {paragraph}
+                  </div>
+                );
+              })}
+            </section>
+          );
+        })}
     </div>
   );
 };
