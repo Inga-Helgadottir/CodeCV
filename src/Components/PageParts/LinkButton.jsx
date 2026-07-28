a785d84a135a27942ccb6d202c032f2ae6833d6d
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-07-28 13:02:22 +0200

    added a prop for target

--- src/Components/PageParts/LinkButton.jsx
+++ src/Components/PageParts/LinkButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../../Styles/LinkButton.css";
 
-const LinkButton = ({ link, text }) => {
+const LinkButton = ({ link, text, target }) => {
   return (
     <button className="linkButton">
-      <a href={link} rel="noreferrer" target="_blank">
+      <a href={link} rel="noreferrer" target={target}>
         {text}
       </a>
     </button>
