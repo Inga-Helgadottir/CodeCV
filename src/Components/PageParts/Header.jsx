4627f7970aa78d1d5b78387842ff90623826b30a
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-07-30 13:28:17 +0200

    changed the name of my cv picture

--- src/Components/PageParts/Header.jsx
+++ src/Components/PageParts/Header.jsx
@@ -1,6 +1,6 @@
 import "../../Styles/Header.css";
 import "../../Styles/ColorVariables.css";
-import imgOfMe from "../../Images/CVpic.jpg";
+import imgOfMe from "../../Images/CV-pic.jpg";
 import UpdateLanguage from "../../Components/PageParts/UpdateLanguage";
 import { getText } from "../../Components/GetCurrentLanguage";
 import { useEffect, useState } from "react";
