d9eaf9aa3b41b63cedf0bb223e93dd2cfd36fa45
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2026-07-30 13:40:36 +0200

    updated NoPage with a prettier button to the startpage

--- src/Components/Pages/NoPage.jsx
+++ src/Components/Pages/NoPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getText } from "../GetCurrentLanguage";
 import "../../Styles/NoPage.css";
+import LinkButton from "../PageParts/LinkButton";
 
 const NoPage = (currentLanguage) => {
   const [noPageText, setNoPageText] = useState("");
@@ -14,7 +15,7 @@ const NoPage = (currentLanguage) => {
     <div className="noPageContainer">
       <h2>{noPageText.heading}</h2>
       <img src="https://http.cat/404" alt="cat" />
-      <a href="/">{noPageText.content}</a>
+      <LinkButton link={"/"} text={noPageText.content} target={""} />
     </div>
   );
 };
